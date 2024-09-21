fff42383b0c840c22e15311fa1244c94b377fac1
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-09-21 23:43:33 +0530

    Update logout.php

--- practice/project1/logout.php
+++ practice/project1/logout.php
@@ -5,15 +5,6 @@ session_start();
 // Unset all session variables
 $_SESSION = array();
 
-// Delete the session cookie if it exists
-// if (ini_get("session.use_cookies")) {
-//     $params = session_get_cookie_params();
-//     setcookie(session_name(), '', time() - 42000,
-//         $params["path"], $params["domain"],
-//         $params["secure"], $params["httponly"]
-//     );
-// }
-
 // Destroy the session
 session_destroy();
 
