6848f3e0647c52f0f7b6ad91c287a2514fadacd5
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-10-05 23:45:28 +0530

    Update basicCalculator.php

--- practice/basicCalculator.php
+++ practice/basicCalculator.php
@@ -21,7 +21,9 @@
 if(isset($_POST['submit'])){
 
     $inp1=isset($_POST["input1"])? (float)$_POST["input1"]:0.0;
+    echo $inp1;
     $inp2=isset($_POST["input2"])? (float)$_POST["input2"]:0.0;
+    echo $inp2;
     echo "Answer is ",$inp1+$inp2;
 }
     
