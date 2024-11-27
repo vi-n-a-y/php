d417a5b13ef87153e3a12856fd9b61b41b005bb5
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-11-27 23:57:24 +0530

    Update userRegistration.php

--- project-starter-code/userRegistration.php
+++ project-starter-code/userRegistration.php
@@ -168,7 +168,9 @@ function cf7_custom_page_html() {
         echo '<td>' . esc_html($submission->state) . '</td>';
         echo '<td>' . esc_html($submission->zip) . '</td>';
         echo '<td>';
-        echo '<button type="button" class="edit-button" data-id="' . esc_attr($submission->id) . '">Edit</button>';
+        echo '<button type="button" class="edit-button" data-id="' . esc_attr($submission->id) . '"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16" style="fill: blue;">
+            <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
+        </svg></button>';
         echo '<button type="button" class="delete-button" data-id="' . esc_attr($submission->id) . '" style="margin-left: 8px;">Delete</button>';
         echo '<form method="POST" style="display:inline;" class="delete-form" action="' . admin_url('admin.php?page=cf7-custom-submissions') . '">';
         wp_nonce_field('cf7_custom_action');
