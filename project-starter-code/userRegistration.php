6b31248f8ce62fcf027b4e7aa7a3ba54dc985143
vinay <149871925+vi-n-a-y@users.noreply.github.com>
2024-12-03 23:52:35 +0530

    Update userRegistration.php

--- project-starter-code/userRegistration.php
+++ project-starter-code/userRegistration.php
@@ -151,7 +151,7 @@ function cf7_custom_page_html() {
     echo '<button type="submit" class="add-up-btn"  style="background-color:blue; margin-left:50px; color:white;  border:none; padding:10px 15px;" name="save_user" id="submit-button">Add User</button>';
     echo '</form>';
 
-    echo '<h2>Existing Submissions</h2>';
+    echo '<h2 style="text-align:center;">Existing Submissions</h2>';
     echo '<table class="widefat fixed" cellspacing="0">';
     echo '<thead><tr>';
     echo '<th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone</th><th>City</th><th>State</th><th>Zip</th><th>Actions</th>';
